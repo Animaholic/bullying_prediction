4e8272418178bddd07a0628f7489db621ab68ed0
Animaholic <shenguohao77@gmail.com>
2023-06-15 10:12:58 -0400

    initial data preprocessing

--- project_code.R
+++ project_code.R
@@ -1,8 +1,27 @@
 ### Authors: Hongmin Huang, Guohao Shen ###
 
-# load the dataset
+## load the dataset
 df <- read.csv("project_data.csv", na.strings = "?")
+
+# get the number of missing values
 sum(is.na(df))
 
-# data preprocessing
+
+## data preprocessing
+
+library(modeest)
+
+# identify columns with missing values
+na_cols <- colnames(df)[colSums(is.na(df)) > 0]
+
+# replace missing values with column modes
+for (col in na_cols) {
+  df[[col]][is.na(df[[col]])] <- mfv(df[[col]], na_rm = TRUE)
+}
+
+# get the number of missing values
+sum(is.na(df))
+
+#write.csv(df, "preprocessed_data.csv", row.names = FALSE)
+
 
