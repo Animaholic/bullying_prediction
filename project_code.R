a9835ca28a7711e402bac34b06646654fe90c3a5
edgivo <136736189+edgivo@users.noreply.github.com>
2023-07-16 18:03:14 -0400

    Update project_code.R

--- project_code.R
+++ project_code.R
@@ -110,25 +110,84 @@ set.seed(31)
 train_control <- trainControl(method = "repeatedcv", number = 10, repeats = 5, 
                               summaryFunction = defaultSummary)
 
-## Model 1: J48
+
+## Model 1: J48 
+install.packages("RWeka")
+library(RWeka)
+
 model_1 <- function(train,test) {
-  set.seed(31)
   j48ml <- J48(class~., data=train)
   predictions <- predict(j48ml, test)
   cm <- confusionMatrix(predictions, test$class)
   return(list(table=cm$table, overall=cm$overall, byClass=cm$byClass))
 }
 
-result <- model_1(train, test)
-result
+result1 <- model_1(train, test)
+result1
 
 ## Model 2: SVM
+install.packages("e1071")
+library(e1071)
+
+tuneGrid <- expand.grid(sigma = seq(0.1, 0.4, by = 0.05), C = seq(1.0, 2.0, by = 0.1))
+
 model_2 <- function(train,test) {
-  svmml <- svm(class ~ ., data = train)
+  svmml <- train(class ~ ., data = train, method = "svmRadial", trControl = train_control, tuneGrid = tuneGrid)
   predictions <- predict(svmml, test)
   cm <- confusionMatrix(predictions, test$class)
   return(list(table=cm$table, overall=cm$overall, byClass=cm$byClass))
 }
 
-result <- model_2(train, test)
-result
+result2 <- model_2(train, test)
+result2
+
+## Model 3: Random Forest
+install.packages("randomForest")
+library(randomForest)
+
+tuneGrid <- expand.grid(.mtry = c(1:10))
+
+model_3 <- function(train, test) {
+  set.seed(31)
+  rf_model <- train(class ~ ., data = train, method = "rf", trControl = train_control, tuneGrid = tuneGrid)
+  predictions <- predict(rf_model, test)
+  cm <- confusionMatrix(predictions, test$class)
+  return(list(table=cm$table, overall=cm$overall, byClass=cm$byClass))
+}
+
+result3 <- model_3(train, test)
+result3
+
+## Model 4: Gradient Boosting
+install.packages("gbm")
+library(gbm)
+
+tuneGrid <- expand.grid(.interaction.depth = c(1, 5, 10), .n.trees = seq(100, 500, by = 50), .shrinkage = c(0.01, 0.1), .n.minobsinnode = 20)
+
+model_4 <- function(train, test) {
+  set.seed(31)
+  gbm_model <- train(class ~ ., data = train, method = "gbm", trControl = train_control, tuneGrid = tuneGrid, verbose = FALSE)
+  predictions <- predict(gbm_model, test)
+  cm <- confusionMatrix(predictions, test$class)
+  return(list(table=cm$table, overall=cm$overall, byClass=cm$byClass))
+}
+
+result4 <- model_4(train, test)
+result4
+
+## Model 5: Logistic Regression
+install.packages("glmnet")
+library(glmnet)
+
+tuneGrid <- expand.grid(.penalty = c(0.01, 0.1, 1, 10, 100), .tol = c(1e-8, 1e-6, 1e-4))
+
+model_5 <- function(train, test) {
+  set.seed(31)
+  log_model <- train(class ~ ., data = train, method = "glmnet", trControl = train_control, tuneGrid = tuneGrid)
+  predictions <- predict(log_model, test)
+  cm <- confusionMatrix(predictions, test$class)
+  return(list(table=cm$table, overall=cm$overall, byClass=cm$byClass))
+}
+
+result5 <- model_5(train, test)
+result5
