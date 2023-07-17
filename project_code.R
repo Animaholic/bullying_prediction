a778b50e9979b03193db6ac6928689fb922ab0ec
Animaholic <shenguohao77@gmail.com>
2023-07-17 00:27:38 -0400

    What's added: 1. over and undersampling 2. data standardization

--- project_code.R
+++ project_code.R
@@ -1,5 +1,6 @@
 ### Authors: Hongmin Huang, Guohao Shen ###
 
+rm(list=ls()); cat("\014") # Clear workspace and console
 # libraries
 library(modeest)
 library(caret)
@@ -11,27 +12,28 @@ library(ggcorrplot)
 library(GGally)
 library(rsample)
 library(e1071)
+library(class)
+library(randomForest)
+library(gbm)
+library(glmnet)
 
 ###############################################################################
 ### data preprocessing
-# load the dataset
-df <- read.csv("project_data.csv", na.strings = "?")
+df <- read.csv("project_data.csv", na.strings = "?") # load the dataset
 dim(df)
 
 # get the number of missing values
 sum(is.na(df))
 
 ## data cleaning
-# identify columns with missing values
-na_cols <- colnames(df)[colSums(is.na(df)) > 0]
+na_cols <- colnames(df)[colSums(is.na(df)) > 0] # identify columns with missing values
 
 # replace missing values with column modes because all attributes are nominal
 for (col in na_cols) {
   df[[col]][is.na(df[[col]])] <- mfv(df[[col]], na_rm = TRUE)
 }
 
-# check the number of missing values is zero
-sum(is.na(df))
+sum(is.na(df)) # check the number of missing values is zero
 
 ## data reduction
 
@@ -40,7 +42,6 @@ df <- df[!duplicated(as.list(df))]
 dim(df)
 
 # dimensionality reduction
-
 # near zero variance
 nearZeroVar(df, names = TRUE)
 df <- df[, -nearZeroVar(df)]
@@ -58,7 +59,7 @@ dim(df)
 # subset <- cfs(class ~., df)
 # df.cfs <- as.simple.formula(subset, "class")
 # df.cfs
-# att1 <- c("vs007","vs030","vs064a","vs066","vs068","vs131","SchCultureRecode")
+att1 <- c("vs007","vs030","vs064a","vs066","vs068","vs131","SchCultureRecode")
 # 
 # # info gain
 # df2 <- copy(df)
@@ -82,15 +83,26 @@ dim(df)
 
 # select important attributes
 # create attCopy for temporary use (no need to run feature selection)
-attCopy <- c("vs064a", "vs066", "vs068", "SchCultureRecode", "class")
+attCopy <- c(att1, "class")
 df <- subset(df, select = attCopy)
 head(df)
+table(df$class)
+
+# over and undersampling
+library(ROSE)
+oversampled_data <- ovun.sample(class ~ ., data = df, method = "both")
+oversampled_data <- oversampled_data$data
+head(oversampled_data)
+
+df <- data.frame(oversampled_data)
+head(df)
+table(df$class)
 
 # save the preprocessed data
 #write.csv(df, "preprocessed_data.csv", row.names = FALSE)
 
 # correlation plot
-sub_df <- subset(df, select = c("vs064a", "vs066", "vs068", "SchCultureRecode"))
+sub_df <- subset(df, select = att1)
 cor(sub_df)
 ggpairs(sub_df)
 ggcorrplot(cor(sub_df), method = "square", lab = TRUE)
@@ -99,9 +111,10 @@ ggcorrplot(cor(sub_df), method = "square", lab = TRUE)
 ###############################################################################
 ### classification
 df$class <- factor(df$class)
+df[1:4] <- scale(df[1:4]) # standardize all 4 features
 
 # train-test split
-split <- initial_split(df, prop = 0.7, strata = class)
+split <- initial_split(df, prop = 0.66, strata = class)
 train <- training(split)
 test <- testing(split)
 
@@ -112,9 +125,6 @@ train_control <- trainControl(method = "repeatedcv", number = 10, repeats = 5,
 
 
 ## Model 1: knn
-install.packages("class")
-library(class)
-
 model_1 <- function(train,test) {
   knn_model <- train(class ~ ., data = train, method = "knn", trControl=train_control, preProcess = c("center", "scale"), tuneLength = 100)
   predictions <- predict(knn_model, test)
@@ -126,9 +136,6 @@ result1 <- model_1(train, test)
 result1
 
 ## Model 2: SVM
-install.packages("e1071")
-library(e1071)
-
 tuneGrid <- expand.grid(sigma = seq(0.1, 0.4, by = 0.05), C = seq(1.0, 2.0, by = 0.1))
 
 model_2 <- function(train,test) {
@@ -142,9 +149,6 @@ result2 <- model_2(train, test)
 result2
 
 ## Model 3: Random Forest
-install.packages("randomForest")
-library(randomForest)
-
 tuneGrid <- expand.grid(.mtry = c(1:10))
 
 model_3 <- function(train, test) {
@@ -159,9 +163,6 @@ result3 <- model_3(train, test)
 result3
 
 ## Model 4: Gradient Boosting
-install.packages("gbm")
-library(gbm)
-
 tuneGrid <- expand.grid(.interaction.depth = c(1, 5, 10), .n.trees = seq(100, 500, by = 50), .shrinkage = c(0.01, 0.1), .n.minobsinnode = 20)
 
 model_4 <- function(train, test) {
@@ -176,9 +177,6 @@ result4 <- model_4(train, test)
 result4
 
 ## Model 5: Logistic Regression
-install.packages("glmnet")
-library(glmnet)
-
 tuneGrid <- expand.grid(.penalty = c(0.01, 0.1, 1, 10, 100), .tol = c(1e-8, 1e-6, 1e-4))
 
 model_5 <- function(train, test) {
